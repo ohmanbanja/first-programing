66e56fcd2b29558d87b2eaa486a193c57fbfb852
ohmanbanja <101543428+ohmanbanja@users.noreply.github.com>
2022-03-28 17:13:35 +0900

    Add files via upload

---
+++ 1.program.py
@@ -0,0 +1,17 @@
+print(1)
+print(2)
+print(3)
+print(1-1)
+print(5+5)
+print(pow(4, 3))
+import random
+print(random.random())
+#sring
+print('hello')
+print("hello")
+print('''
+    hello
+    world
+''')
+print(len('hello'))
+print('hello world')
